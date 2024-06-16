ad8ccd7f9efaeacc0adcfcdd4fe4d84d550d0367
agusmonti <agustinmonti2002@gmail.com>
2024-06-16 20:43:50 -0300

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitan America'];
+  heroeBorrado: string =  '';
+
+  borrarHeroe () {
+
+    // this.heroes.splice(3,1)
+
+    this.heroeBorrado = this.heroes.shift() || '';
+
+  }
+
+}
